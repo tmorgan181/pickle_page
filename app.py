d48ebff8dc9b77caba13ed54b0124cf87f51f628
Nick Buerk <ntbvyc@mst.edu>
2021-10-30 12:59:17 -0500

    Pickles being rendered

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
+import os
+
+images_dir = os.path.join('static', 'Images')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index() -> None:
     title = 'Pickle Page'
-    return render_template('index.html', title=title)
+
+    pickle_files=[]
+    for filename in os.listdir(images_dir):
+        pickle_files.append(os.path.join(images_dir, filename))
+
+    return render_template('index.html', title=title, pickles=pickle_files, len=len(pickle_files))
