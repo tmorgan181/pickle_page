ff3903406428c1802e27937e098f0d52eb405ac1
Trenton Morgan <tpm7qq@mst.edu>
2021-10-30 15:44:46 -0500

    Plays all sounds from the pickle_audio directory

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import os
+import json
 
 images_dir = os.path.join('static', 'Images')
+sounds_dir = os.path.join('static', 'pickle_audio')
 
 app = Flask(__name__)
 
@@ -13,4 +15,10 @@ def index() -> None:
     for filename in os.listdir(images_dir):
         pickle_files.append(os.path.join(images_dir, filename))
 
-    return render_template('index.html', title=title, pickles=pickle_files)
+
+    sound_files=[]
+    for filename in os.listdir(sounds_dir):
+        sound_files.append(os.path.join("..", sounds_dir, filename))
+    print(sound_files)
+
+    return render_template('index.html', title=title, pickles=pickle_files, sound_files=json.dumps(sound_files))
